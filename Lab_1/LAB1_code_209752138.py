11aeb9707e966ccfeef8865d1e2316f077421104
Your Name <you@example.com>
2026-05-29 17:45:27 +0300

    Inclued a function to find the new paramters for the pipe diamter and inlet pressure to preserve the flow rate of original impeller

--- Lab_1/LAB1_code_209752138.py
+++ Lab_1/LAB1_code_209752138.py
@@ -249,7 +249,7 @@ def find_pump_head(impeller_data: np.ndarray, impeller_size:str) -> tuple[np.nda
 
 # ============= Q2: Find the system head and graph the pump and system curve ===================
 
-def find_system_head(F: float) -> float: # Function of flow rate (F) [m^3/s] 
+def find_system_head(F: float) -> float:
     
     """
     Calculates the system head using the known bernoulli equation 
@@ -321,7 +321,8 @@ def find_working_point(impeller_data: np.ndarray, impeller_size: str) -> tuple[f
     Head in csv (input) -> [m]
     Impeller size (input) -> str [inches]
     Flow rate -> [m^3/s]
-    Flow rate (output) -> [m^3/hr]
+    Flow rate at working point (output) -> [m^3/hr]
+    Head at working point (output) -> [m]
     """
     
     delta_H_pump, _ = find_pump_head(impeller_data, impeller_size) # gets polynomial approx for pump head
@@ -346,7 +347,7 @@ def find_working_point(impeller_data: np.ndarray, impeller_size: str) -> tuple[f
 
 # ============= Q1: graph the pump and system curves for all impeller sizes on the same plot =============
 
-def delta_H_pump_for_all_impeller_sizes(impeller_data: np.ndarray) -> dict: # Get the polynomial function for all impeller sizes
+def delta_H_pump_for_all_impeller_sizes(impeller_data: np.ndarray) -> dict:
     
     """
     Calculates the polynomial approximations for the pump heads of each impeller size in the csv.
@@ -369,7 +370,7 @@ def delta_H_pump_for_all_impeller_sizes(impeller_data: np.ndarray) -> dict: # Ge
 
     return impeller_head_functions
 
-def graph_all_impeller_sizes(impeller_data: np.ndarray): # Graph the pump and system curves for all impeller sizes on the same plot
+def graph_all_impeller_sizes(impeller_data: np.ndarray):
     
     """
     Graphs the pump curve and system curve on the same plot for all impeller sizes on a single plot. 
@@ -448,7 +449,7 @@ def graph_all_impeller_sizes(impeller_data: np.ndarray): # Graph the pump and sy
 
 # ============= Q2: Find the working points for all impeller sizes =======================================
 
-def find_working_points_all_impeller_sizes(impeller_data: np.ndarray) -> dict: # Find the working points for all impeller sizes
+def find_working_points_all_impeller_sizes(impeller_data: np.ndarray) -> dict:
     
     """
     Calculates the working points for each impeller size and inserts in a dictionary,
@@ -529,8 +530,6 @@ def Check_NPSH_for_all_impeller_sizes(impeller_data: np.ndarray) -> None:
         NPSH_req = NPSH_Required(F_at_working_point/3600)    #[m^3/hr] -> [m^3/s]
         NPSH_avail = NPSH_Available(F_at_working_point/3600) #[m^3/hr] -> [m^3/s]
         
-        print(f' \nNPSH Required at the working point is {NPSH_req:.3f} [m]')
-        
         if NPSH_avail > NPSH_req:
             print(f'For impeller size {impeller_size} inches: the NPSH Available at the working point is {NPSH_avail:.3f} [m] -> No cavitation risk\n')
         else:
@@ -575,6 +574,236 @@ This results in the lowering of the starting point of the system head curve, whi
 Using these two methods, we can potentially recover the original working point flow rate rate with the 6&7/8" impeller.
 """ 
 
+def find_flow_velocity_modified(F: float) -> float: 
+
+    """
+    Modified flow rate using a larger diameter pipe
+
+    Flow rate units (input) -> [m^3/s]
+    New pipe diameter (input) -> [m]
+    Pipe diameter -> [m]
+    Pipe Area -> [m^2]
+    Average fluid velocity units (output) -> [m/s] 
+    """
+
+    A = math.pi * (D_PIPE_NEW / 2) ** 2 
+    V = F / A 
+    return V 
+
+def find_h_LT_modified(F: float) -> float: 
+
+    """
+    Finds the modified h_LT using the new larger pipe diameter
+
+    Flow rate units (input) -> [m^3/s]
+    New pipe diameter (nput) -> [m]
+    Average fluid velocity -> [m/s]
+    Reynolds -> [no units]
+    Fanning friction factor -> [no units]
+    Absolute pipe roughness / Epsilon -> [m]
+    Pipe diameter -> [m]
+    Pipe length -> [m]
+    No. of turns & turn friction factor -> [no units]
+    Total frictional head loss -> [m^2/s^2]
+    """
+
+    V = find_flow_velocity(F) 
+    Re = find_reynolds_number(F) 
+    eD = EPSILON / D_PIPE_NEW 
+    ff = f_fanning(Re, eD) 
+     
+    h_LT = (ff * (L_PIPE / D_PIPE_NEW) + 
+            NUMBER_OF_TURNS * TURN_FRICTION_FACTOR) * 2 * V ** 2 
+    
+    return h_LT
+
+def find_system_head_modified(F: float) -> float:
+    
+    """
+    Calculates the system head using the known bernoulli equation 
+    with the frictional head losses function.
+    Here we use the modified parameters to try and change to working point of the 5&11/16" impeller
+    to the working point of 6&7/8" impeller (in terms of flow rate)
+    
+    ---- Units -----
+    Flow rate (input) -> [m^3/s]
+    New pipe diameter (input) -> [m]
+    New inlet pressure (input) -> [Pa]
+    H_system (output) -> [m]
+    """
+
+    v_ave_in = 0 # fluid at top of tank is static [m/s]
+    v_ave_out = find_flow_velocity_modified(F) # Average flow velocity [m/s]
+    h_LT = find_h_LT_modified(F) # Frictional head loss [m^2/s^2]
+
+    H_system = (P_OUT - P_IN_NEW) / (RHO * g) + DELTA_HEIGHT + (KINETIC_ALPHA * (v_ave_out**2 - v_ave_in**2) / (2 * g) + (h_LT / g))  # Total system head
+
+    return H_system
+
+def find_working_point_modified(impeller_data: np.ndarray, impeller_size: str) -> tuple[float, float]: 
+    
+    """
+    Calculates the working point for the modified system (different pressure at inlet and pipe diameter)
+
+    ----- Units -----
+    Flow rate in csv (input) -> [m^3/hr]
+    Head in csv (input) -> [m]
+    New pipe diameter (input) -> [m]
+    New inlet pressure (input) -> [Pa]
+    Impeller size (input) -> str [inches]
+    Flow rate -> [m^3/s]
+    Flow rate (output) -> [m^3/hr]
+    """
+    
+    delta_H_pump, _ = find_pump_head(impeller_data, impeller_size) # gets polynomial approx for pump head
+
+    def head_difference(F: float) -> tuple[np.ndarray, np.ndarray]: # head_difference = delta_H_pump - delta_H_system
+
+        H_pump = delta_H_pump(F) # Flow in [m3/s]
+        H_system = find_system_head_modified(F) # Flow in [m3/s]
+        
+        return H_pump - H_system 
+    
+    F_at_working_point = fsolve(head_difference, x0=0.005, xtol=0.001) # Head_difference and x0 in [m3/s]
+    Head_at_working_point = delta_H_pump(F_at_working_point) # F_at_working_point in [m3/s]
+    
+    return F_at_working_point*3600, Head_at_working_point # Convert flow rate units [m3/s] -> [m3/h] 
+
+def find_new_parameters(impeller_data: np.ndarray) -> tuple[float, float]:
+    """
+    Calculates the new pressure difference and pipe diameter needed to keep the flow rate the same for the
+    defective 6&7/8" impeller that works like a 5&11/16" impeller
+    """
+    global D_PIPE_NEW, P_IN_NEW # define the new paramters as global parameters
+    P_IN_NEW = P_IN
+    D_PIPE_NEW = D_PIPE
+
+    F_at_working_point_6_and_7_8, _ = find_working_point(impeller_data, '6&7/8')
+    F_at_working_point_5_and_11_16, _ = find_working_point(impeller_data, '5&11/16')
+
+    F_diff = (F_at_working_point_6_and_7_8 - F_at_working_point_5_and_11_16)
+    F_tol = 0.01 
+    iter, curr_iter = 1e5, 0
+    
+    incr_P = 100 # [Pa]
+    incr_D = 0.0005 # [m]
+
+    while (abs(F_diff) > F_tol) and (curr_iter <= iter):
+        if F_diff > 0:
+            P_IN_NEW += incr_P
+            D_PIPE_NEW += incr_D
+            
+            if P_IN_NEW <= 100:
+                incr_P = 0
+            if D_PIPE_NEW <= 5e-4:
+                incr_D = 0
+            
+            F_current, _ = find_working_point_modified(impeller_data, '5&11/16')
+            F_diff = (F_at_working_point_6_and_7_8 - F_current)
+        else:
+            P_IN_NEW -= incr_P
+            D_PIPE_NEW -= incr_D
+            
+            incr_P /= 2
+            incr_D /= 2
+        curr_iter += 1
+
+    return D_PIPE_NEW, P_IN_NEW
+
+def graph_all_impeller_sizes_modified(impeller_data: np.ndarray) -> None:
+    
+    """
+    Creates the graph for the original system, and then adds the system curve for the
+    modified system (new pressure and pipe diameter)
+   
+    ----- Units -----
+    Flow rate in csv (input) -> [m^3/hr]
+    Head in csv (input) -> [m]
+    Impeller size (input) -> str [inches]
+    Flow rate -> [m^3/s]
+    Flow rate in graph -> [m^3/hr]
+    Head in graph -> [m]
+    """
+    
+    delta_H_pump_functions = delta_H_pump_for_all_impeller_sizes(impeller_data) # Dictionary of polynomials for all impeller sizes 
+    
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+
+    eff_label_added = False
+    
+    efficiency_list = import_efficiency_data() # Import the efficiency data for all impeller sizes from the CSV file
+    for eff, (x_eff, y_eff) in efficiency_list.items(): # Plot the efficiency curves for all impeller sizes
+        
+        eff_label = 'efficiencies' if not eff_label_added else '_nolegend_' # Single label for the pump curve in the legend
+        eff_label_added = True
+        
+        ax1.plot(x_eff, y_eff, label=eff_label, color='blue') # Plot efficiency curve
+        ax1.annotate(f'{eff}', xy=(x_eff[0], y_eff[0]), xytext=(x_eff[0],y_eff[0]), fontsize=9, color='black')
+
+
+    H_pump_label_added = False #  Check if label for the pump curve is added to the legend
+    max_F = 65 # [m^3/hr] Maximum flow rate for plotting, based on the test cases
+
+    for impeller_size, delta_H_pump in delta_H_pump_functions.items():
+    
+        F_values = impeller_data[:, impeller_size_dict[impeller_size]]  # Flow rates from csv data for the specified impeller size [m^3/hr]
+        F_values = F_values.copy() # Create a copy of the flow rate values to modify for plotting
+        F_values = F_values[F_values <= max_F]  # Filter flow rates to be within the maximum flow rate for plotting
+        F_values[-1] = max_F 
+
+        H_pump = [delta_H_pump(F/3600) for F in F_values] # Calculate pump head for each flow rate (Flow rate converted to [m^3/s])
+        H_system = [find_system_head(F/3600) for F in F_values] # Calculate system head for each flow rate (Flow rate converted  to [m^3/s])
+        working_point = find_working_point(impeller_data, impeller_size) # Find the working point of the pump
+
+        pump_label = '▲Hpump' if not H_pump_label_added else '_nolegend_' # Single label for the pump curve in the legend
+        H_pump_label_added = True
+        
+        ax1.plot(F_values, H_pump, label=pump_label, color='red') # Plot pump curve
+        ax1.scatter(working_point[0], working_point[1], color='black', zorder=5) # Plot the working point
+        ax1.annotate(f'{impeller_size}"', xy=(0, H_pump[0]), xytext=(0,H_pump[0]+1), fontsize=10, color='black')
+    
+    ax1.plot(F_values, H_system, label='▲Hsys', color='green') # Plot system curve
+
+    H_system_modified = [find_system_head_modified(F/3600) for F in F_values]
+    modified_working_point = find_working_point_modified(impeller_data, '5&11/16')
+
+    ax1.plot(F_values, H_system_modified, label='Modified ▲Hsys', color='green', linestyle='--') # Plot the modifed system curve
+    
+    ax1.annotate(f'$\\mathbf{{{'System \\ modification'}}}$\n New Pipe Diameter: {D_PIPE_NEW:.3f} [m]\n New Inlet Pressure: {P_IN_NEW:.3f} [Pa]',
+                 xy=(F_values[0],H_system_modified[0]), xytext=(0,3),
+                 fontsize=10, color='black', bbox=dict(
+                boxstyle='round,pad=0.3',    
+                facecolor='lightblue',           
+                edgecolor='gray',            
+                alpha=0.9,                   
+                linewidth=0.5))
+
+    ax1.scatter(modified_working_point[0], modified_working_point[1], color='black', zorder=5) # Plot the working point of the modified system with 5&11/16"
+    ax1.axvline(modified_working_point[0], color='black', linestyle=':')
+    ax1.annotate('Q of modified working point', xy=(modified_working_point[0],75), 
+                 xytext=(modified_working_point[0]-10,75), fontsize=10, color='black', 
+                 ha='center', va='center', arrowprops=dict(facecolor='black', shrink=0.01))
+
+    ax1.set_xlim(-3, 67)
+    ax1.set_ylim(0, 80)
+
+    ax2 = ax1.twiny() # Create a second x-axis for the system curve
+    ax2.set_xscale('log') # Set the second x-axis to logarithmic scale
+    ax2.set_xlim(0.5,17) # Set the limits of the second x-axis to match the tests
+
+    ax1.set_xlabel('Q (m3/hr)')
+    ax1.set_ylabel('h (m)')
+    ax2.set_xlabel('NPSH Required (m)')
+
+    ax2.set_xticks([2, 4, 6, 8, 10, 12, 14, 16]) # NPSH_R values for the axis as seen in the tests
+    ax2.set_xticklabels(['2', '4', '6', '8', '10', '12', '14', '16'])
+    
+    plt.title(f'Pump and System Curves for all Impeller Sizes\n with a modification for pipe diameter and inlet pressure')
+    ax1.legend(loc='lower right')
+    ax1.grid()
+    plt.tight_layout()
+    plt.show()
+
 
 # ------ Checks for PartA ------
 
@@ -584,7 +813,6 @@ def Check_PartA_Q2():
 def Check_PartA_Q3():
     h_LT_value = find_h_LT(F)
     print(f'Frictional head loss (h_LT) for flow rate F={F:.3f} [m^3/s]: {h_LT_value:.3f} [m^2/s^2]\n')
-
 
 # ----- Checks for PartB -----
 
@@ -596,8 +824,6 @@ def Check_PartB_Q3():
     working_point = find_working_point(impeller_data, "6") # Check the working point for the 6 inch impeller
     F_at_working_point, Head_at_working_point = working_point[0].item(), working_point[1].item()
     print(f'The flow rate at the working point is {F_at_working_point:.3f} [m^3/hr] \nThe head at the working point is {Head_at_working_point:.3f} [m]\n') 
-
-
 
 # ----- Checks for PartC ------
 
@@ -614,13 +840,22 @@ def Check_PartC_Q2():
 def Check_PartC_Q3():
     Check_NPSH_for_all_impeller_sizes(impeller_data)
 
+# ----- Checks for Open Question -----
+def Check_Open_Q():
+    find_new_parameters(impeller_data)
+    graph_all_impeller_sizes_modified(impeller_data)
+
+
 """
 Main Pipline to run the code when opened
 """
+
 if __name__ == '__main__':
     
-    Check_PartA_Q2(), Check_PartA_Q3()
-    Check_PartB_Q2(), Check_PartB_Q3()
-    Check_PartC_Q1(), Check_PartC_Q2(), Check_PartC_Q3()
+    #Check_PartA_Q2(), Check_PartA_Q3()
+    #Check_PartB_Q2(), Check_PartB_Q3()
+    #Check_PartC_Q1(), Check_PartC_Q2(), Check_PartC_Q3()
+    Check_Open_Q()
+
 
 
